a0e493fd2874dce3e9448fd981d012d7dc6f84bb
James Marques <jamzmarks@gmail.com>
2025-11-04 07:29:31 -0300

    feat: update gateway

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ apk update && apk add curl
 apk add --no-cache perl
 
 resty -e "local jwt = require 'resty.jwt'; print('JWT OK')"
+
+//start
+Start-Service -Name Cloudflared
+Get-Service -Name Cloudflared | Format-List *
+# ou (resumo)
+Get-Service -Name Cloudflared
+Set-Service -Name Cloudflared -StartupType Automatic
+Get-Process -Name cloudflared -ErrorAction SilentlyContinue
+
+// nome do serviço rodando
+Get-WmiObject -Class Win32_Service -Filter "Name='Cloudflared'" | Select-Object Name, PathName, StartMode, State | Format-List
+
+ cloudflared tunnel run --url https://localhost:8443 --token eyJhIjoiM2IwMzFiYTZjMWIyYzExNmQzMjFjZTNkNDUwMzZjOTAiLCJ0IjoiOGNiMTIyNDMtM2RkMi00NjU4LWI2NmEtMDFiY2U0YTk1NDFjIiwicyI6Ik9EYzNOR0pqWXpFdE1qRTNNeTAwWmpBNUxXRmxNRFF0TURVNVltTTBNV1ZrTURObCJ9
--- lua/routes.lua
+++ lua/routes.lua
@@ -1,27 +1,53 @@
 local uri = ngx.var.request_uri
 
-local administration_url = os.getenv("ADMIN_API_URL") or "https://host.docker.internal:4000/api/v1/"
-local devices_url = os.getenv("DEVICES_API_URL") or "http://host.docker.internal:3005/api/v1/"
-local metrics_url = os.getenv("METRICS_API_URL") or "http://host.docker.internal:3006/api/v1/"
+-- URLs padrão (caso variáveis de ambiente não estejam setadas)
+local ADMIN_API_URL = os.getenv("ADMIN_API_URL") or "http://host.docker.internal:4000/api/v1/"
+local DEVICES_API_URL = os.getenv("DEVICES_API_URL") or "http://host.docker.internal:3005/api/v1/"
+local METRICS_API_URL = os.getenv("METRICS_API_URL") or "http://host.docker.internal:3006/api/v1/"
+
+
+local ADMIN_API_DOCS = os.getenv("ADMIN_API_DOCS") or "http://host.docker.internal:4000/docs"
+local DEVICES_API_DOCS = os.getenv("DEVICES_API_DOCS") or "http://host.docker.internal:3005/docs"
+local METRICS_API_DOCS = os.getenv("METRICS_API_DOCS") or "http://host.docker.internal:3006/docs"
 
 local target_base
 local path = uri
 
+-- 🔹 Roteamento principal
 if uri:match("^/ad") then
-    target_base = administration_url
-    path = uri:gsub("^/ad", "")
+    if uri:match("^/ad/docs") then
+        target_base = ADMIN_API_DOCS
+        path = uri:gsub("^/ad/docs", "")
+    else
+        target_base = ADMIN_API_URL
+        path = uri:gsub("^/ad", "")
+    end
+
 elseif uri:match("^/dv") then
-    target_base = devices_url
-    path = uri:gsub("^/dv", "")
+    if uri:match("^/dv/docs") then
+        target_base = DEVICES_API_DOCS
+        path = uri:gsub("^/dv/docs", "")
+    else
+        target_base = DEVICES_API_URL
+        path = uri:gsub("^/dv", "")
+    end
+
 elseif uri:match("^/mt") then
-    target_base = metrics_url
-    path = uri:gsub("^/mt", "")
+    if uri:match("^/mt/docs") then
+        target_base = METRICS_API_DOCS
+        path = uri:gsub("^/mt/docs", "")
+    else
+        target_base = METRICS_API_URL
+        path = uri:gsub("^/mt", "")
+    end
+
 else
     ngx.status = 404
     ngx.say("Route not found")
     return ngx.exit(404)
 end
 
+-- 🔹 Montagem final da rota alvo
 ngx.var.target = target_base:gsub("/$", "") .. path
 ngx.log(ngx.ERR, "Roteando para target: ", ngx.var.target)
 
